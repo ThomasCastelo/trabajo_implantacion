53393379e5d3a5e3e70b359da37bea9e85e9c71b
Thomas Castelo <thomas.castelo.dosa@gmail.com>
2026-02-09 13:58:56 +0100

    Fix: Eliminar textos de prueba del menú lateral

--- run_migration.ps1
+++
@@ -1,6 +0,0 @@
-$mysqlPath = "C:\Program Files\MySQL\MySQL Server 8.0\bin\mysql.exe"
-$sqlFile = "C:\implantacion\fastapi_plantillascomunes\sql\add_edit_and_votes.sql"
-$password = Read-Host "Ingresa la contraseña de MySQL" -AsSecureString
-$plainPassword = [System.Runtime.InteropServices.Marshal]::PtrToStringAuto([System.Runtime.InteropServices.Marshal]::SecureStringToCoTaskMemUnicode($password))
-
-& $mysqlPath -u root -p"$plainPassword" -e "USE thomas; source '$sqlFile'"
